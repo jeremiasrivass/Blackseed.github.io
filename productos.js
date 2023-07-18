39e55bb7b5226ba7ccf3838add959c1ef43ac105
jeremiasrivass <118233061+jeremiasrivass@users.noreply.github.com>
2023-07-18 12:39:29 -0300

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,63 @@
+'use strict';
+
+    const productos= [
+{
+   Nombre :'Java House 1KG',
+   Precio:10595,
+   Descripcion: 'Esta mezcla brillante y suave combina la acidez de la manzana fresca con la dulzura del caramelo especiado.Grano suave.',
+   imagen: 'producto1.png',
+    },
+    {
+        Nombre: 'Jablum Jamaica 1KG',
+        Precio:8596,
+        Descripcion:'Suavidad sedosa,bien equilibrado,con cuerpo,dulce y cremoso,y con un sabor suave en general.',
+        imagen: 'producto2.png',
+    },
+    {
+        Nombre: 'Juan Valdez 1KG',
+        Precio: 6470,
+        Descripcion:'Posee un punto de tostado medio alto lo que le otorga la capacidad de sentirse inmediatamente en el paladar.',
+        imagen: 'producto3.png',
+    },
+    {
+        Nombre: 'Death Wish 1KG',
+        Precio: 7999,
+        Descripcion:'Profundo y muy fuerte, no hay sutilezas en el paladar al beber cada trago de este café. ',
+        imagen: 'producto4.png',
+    },
+
+    {
+        Nombre: 'Volcanica 1KG',
+        Precio: 7156,
+        Descripcion:' Se distingue por sabor suave, intenso aroma y cuerpo robusto. Con toques avellanados y dulces.  ',
+        imagen: 'producto5.png',
+    },
+    {
+        Nombre: 'SB Kenya 1KG',
+        Precio: 4556,
+        Descripcion:'Café de origen con un tostado medio y sabores cítricos en sus principales notas.',
+        imagen: 'producto6.png',
+    },
+    {
+        Nombre: 'Peet´s 1KG',
+        Precio: 5896,
+        Descripcion:'Rico y destacado con crema deliciosa que rodea un núcleo picante.',
+        imagen: 'producto7.png',
+    },
+  
+    {
+        Nombre: 'Cubico Black 1KG',
+        Precio: 7758,
+        Descripcion:' Café de gran fragancia fruta con notas cerezas y uvas negras. Café de especialidad, 100% arábico.',
+        imagen: 'producto8.png',
+    },
+  
+    {
+        Nombre: 'Wild Jo 1KG',
+        Precio: 9878,
+        Descripcion:'Orgánico tostado francés oscuro, café molido, fuerte y rico café malvado, gran café elaborado.',
+        imagen: 'producto9.png',
+    },
+  
+]
+
